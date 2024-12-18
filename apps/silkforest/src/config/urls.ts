eae6d9c3351a9b8b86d65637c10d1e44e9912e70
heidaraliy <heidar.aliy@gmail.com>
2024-12-17 21:35:54 -0500

    adjust url path

--- apps/silkforest/src/config/urls.ts
+++ apps/silkforest/src/config/urls.ts
@@ -1,11 +1,11 @@
-const isDevelopment = process.env.NODE_ENV === 'development';
+const isDevelopment = process.env.NODE_ENV === "development";
 
-const BASE_URL = isDevelopment 
-  ? 'http://localhost:3000' 
-  : 'https://silkforest.app';
+const BASE_URL = isDevelopment
+  ? "http://localhost:3000"
+  : "https://silkforest.xyz";
 
 export const URLS = {
   docs: (section: string) => `${BASE_URL}/docs#${section}`,
   downloads: (product: string) => `${BASE_URL}/downloads/${product}`,
   home: () => BASE_URL,
-}; 
+};
